d4ecb32bf586f3aa3bef99fba4b16986137047dc
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-20 12:22:29 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// gsap.from(".box1",{
+//     x: 1700,
+//     duration: 2,
+//    delay:1,
+//    backgroundColor: "red",
+//    borderRadius: "50%",
+//    scale: 0.5,
+// //    repeat:-1,
+// //    yoyo: true,
+// })
+// gsap.from(".box2",{
+//     x: 1700,
+//     duration: 4,
+//    delay:1,
+//    backgroundColor: "red",
+//    borderRadius: "50%",
+//    scale: 0.5,
+// //    repeat:-1,
+// //    yoyo: true,
+// })
+// gsap.from(".box3",{
+//     x: 1700,
+//     duration: 6,
+//    delay:1,
+//    backgroundColor: "red",
+//    borderRadius: "50%",
+//    scale: 0.5,
+// //    repeat:-1,
+// //    yoyo: true,
+// })
+
+var v1=gsap.timeline();
+v1.from(".box1",{
+    x: 1700,
+    duration: 2,
+   delay:1,
+   backgroundColor: "red",
+   borderRadius: "50%",
+   scale: 0.5,
+//    repeat:-1,
+//    yoyo: true,
+})
+v1.from(".box2",{
+    x: 1700,
+    duration: 2,
+   delay:1,
+   backgroundColor: "red",
+   borderRadius: "50%",
+   scale: 0.5,
+//    repeat:-1,
+//    yoyo: true,
+})
+v1.from(".box3",{
+    x: 1700,
+    duration: 2,
+   delay:1,
+   backgroundColor: "red",
+   borderRadius: "50%",
+   scale: 0.5,
+//    repeat:-1,
+//    yoyo: true,
+})
